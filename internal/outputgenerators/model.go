bbeb8d463647e111e599cd354be45849bd4e4b18
Erik Borowski <erik@ebinf.eu>
2022-12-19 20:26:32 +0100

    Added OUTPUT_FOLDER constant, updated debug

--- internal/outputgenerators/model.go
+++ internal/outputgenerators/model.go
@@ -2,6 +2,8 @@ package outputgenerators
 
 import "github.com/stg-tud/bp2022_netlab/internal/experiment"
 
+const OUTPUT_FOLDER = "output"
+
 type OutputGenerator interface {
 	Generate(experiment.Experiment)
 }
