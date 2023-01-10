7a3be583dd164b7417e0ee3ff7c166a9ae86c152
Wang <117817115+Wang1872@users.noreply.github.com>
2023-01-10 12:41:16 +0100

    Bug fix load.go

---
+++ internal/experiment/load.go
@@ -0,0 +1,28 @@
+package experiment
+
+import (
+	"os"
+	"github.com/pelletier/go-toml/v2"
+)
+
+// loads the path string with toml file into experiment and returns experiment
+
+func Loading(path string) Experiment {
+	var exp Experiment
+	doc, e := os.ReadFile(path)
+	if e != nil {
+		panic(e)
+	}
+
+	err := toml.Unmarshal(doc, &exp)
+
+	if err != nil {
+		panic(err)
+	}
+	var nodegroups []NodeGroup
+	for i, _ := range exp.NodeGroups {
+		nodegroups = append(nodegroups, NewNodeGroup(exp.NodeGroups[i].Prefix, exp.NodeGroups[i].NoNodes))
+	}
+	exp.NodeGroups = nodegroups
+	return exp
+}
