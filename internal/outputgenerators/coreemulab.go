4a8f7f408eb5a8f5596581f8b038dea9cfd1b1d4
wang <jw117010@gmail.com>
2023-01-13 14:03:20 +0100

    Coreemulab.go with test and example.conf

--- internal/outputgenerators/coreemulab.go
+++ internal/outputgenerators/coreemulab.go
@@ -1,1 +1,49 @@
 package outputgenerators
+
+import (
+	"encoding/xml"
+	"os"
+)
+
+const (
+	Header = `<?xml version="1.0" encoding="UTF-8"?>` + "\n"
+)
+
+type CoreEmulab struct{}
+
+type Scenario struct {
+	XMLName  xml.Name  `xml:"scenario"`
+	Networks []Network `xml:"networks`
+}
+
+type Network struct {
+	XMLName xml.Name `xml:"network"`
+	Id      int      `xml:"id,attr"`
+	Name    string   `xml:"name,attr"`
+	Type    string   `xml:"type,attr"`
+}
+
+// generates a XML and a conf configuartion for CoreEmulab with a given experiment
+func (c CoreEmulab) Generate() {
+
+	nt := Network{
+		Id:   1,
+		Name: "AdHoc",
+		Type: "WIRELESS",
+	}
+	scenario := &Scenario{}
+	scenario.Networks = []Network{nt}
+
+	data, err := os.Create("coreemulab.xml")
+	if err != nil {
+		panic(err)
+	}
+	data.WriteString(xml.Header)
+	encoder := xml.NewEncoder(data)
+	encoder.Indent("", "\t")
+	err = encoder.Encode(&scenario)
+	if err != nil {
+		panic(err)
+	}
+
+}
--- test/coreemulab_test.go
+++ test/coreemulab_test.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"testing"
+
+	"github.com/stg-tud/bp2022_netlab/internal/outputgenerators"
 )
 
 func TestOutput(t *testing.T) {
 
+	core := outputgenerators.CoreEmulab{}
+	core.Generate()
 }
