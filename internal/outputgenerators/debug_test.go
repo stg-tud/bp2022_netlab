e2a496b580e0dbdb8ece9a4018de34b17ca12cf6
Erik Borowski <erik@ebinf.eu>
2023-01-30 16:29:35 +0100

    Fixed line break issue on windows

--- internal/outputgenerators/debug_test.go
+++ internal/outputgenerators/debug_test.go
@@ -3,6 +3,7 @@ package outputgenerators_test
 import (
 	"os"
 	"path/filepath"
+	"strings"
 	"testing"
 
 	"github.com/stg-tud/bp2022_netlab/internal/outputgenerators"
@@ -20,13 +21,15 @@ func TestDebugGeneration(t *testing.T) {
 	if err != nil {
 		t.Fatal("Could not read output file", err)
 	}
+	expectedClean := strings.ReplaceAll(string(expected), "\r\n", "\n")
 
 	actual, err := os.ReadFile(filepath.Join(outputgenerators.OutputFolder, outputgenerators.DebugOutputFile))
 	if err != nil {
 		t.Fatal("Could not read output file", err)
 	}
+	actualClean := strings.ReplaceAll(string(actual), "\r\n", "\n")
 
-	if string(actual) != string(expected) {
+	if string(actualClean) != string(expectedClean) {
 		t.Fatal("Output does not match expected output!")
 	}
 }
