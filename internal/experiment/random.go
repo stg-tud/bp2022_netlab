85d21431a699fdd2d1fd59ab325c24f654eb78a7
Erik Borowski <erik@ebinf.eu>
2023-01-31 02:14:41 +0100

    Introduced logging

--- internal/experiment/random.go
+++ internal/experiment/random.go
@@ -3,10 +3,13 @@ package experiment
 import (
 	"math/rand"
 	"time"
+
+	logger "github.com/gookit/slog"
 )
 
 // GenerateRandomSeed returns a random seed used for random generations
 func GenerateRandomSeed() int64 {
+	logger.Trace("Generating new random seed")
 	time.Sleep(1000) // Sleep a few nanoseconds in order for Windows to update its time and allow unique timestamps
 	source := rand.NewSource(time.Now().UnixNano())
 	randObj := rand.New(source)
@@ -16,6 +19,7 @@ func GenerateRandomSeed() int64 {
 
 // GetRandomSource returns a randomness Rand object for the given randomSeed
 func GetRandomSource(randomSeed int64) *rand.Rand {
+	logger.Trace("Generating new random source")
 	source := rand.NewSource(randomSeed)
 	return rand.New(source)
 }
