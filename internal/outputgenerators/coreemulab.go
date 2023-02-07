4e7bf930a255d1978110963d3ae251390b719912
JulianDao4 <juliandao24@gmail.com>
2023-02-07 19:34:05 +0100

    minor fixes

--- internal/outputgenerators/coreemulab.go
+++ internal/outputgenerators/coreemulab.go
@@ -5,7 +5,9 @@ import (
 	"path/filepath"
 	"text/template"
 
+	logger "github.com/gookit/slog"
 	"github.com/stg-tud/bp2022_netlab/internal/experiment"
+	"github.com/stg-tud/bp2022_netlab/internal/folderstructure"
 )
 
 type CoreEmulab struct{}
@@ -31,17 +33,18 @@ type data struct {
 
 // generates a XML and a conf configuartion for CoreEmulab with a given experiment
 func (c CoreEmulab) Generate(exp experiment.Experiment) {
+	logger.Info("Generating CORE output")
+	outputFolder, err := folderstructure.GetAndCreateOutputFolder(exp)
+	os.Mkdir(outputFolder, 0755)
 
-	os.Mkdir(OutputFolder, 0755)
-
-	fbuffer, err := os.Create(filepath.Join(OutputFolder, "experiment.conf"))
+	fbuffer, err := os.Create(filepath.Join(outputFolder, "experiment.conf"))
 	if err != nil {
 		panic(err)
 	}
 	replace := data{
 		Name: exp.Name,
 
-		Runtime: exp.Duration,
+		Runtime: int(exp.Duration),
 	}
 
 	confTemplate, err := template.ParseFiles(filepath.Join(GetTemplatesFolder(), "experiment.conf"))
