545cd70dcb15bbf5a2b2a29bba684fbe92e2a26c
Dao <juliandao24@gmail.com>
2023-03-18 20:50:22 +0100

    Undid some changes

--- internal/outputgenerators/model_test.go
+++ internal/outputgenerators/model_test.go
@@ -89,10 +89,6 @@ func GetTestingExperiment() experiment.Experiment {
 			Height: 170,
 			Width:  240,
 		},
-		/*ExternalMovement: experiment.ExternalMovement{
-			Active:   true,
-			FileName: "three_nodes.pos",
-		},*/
 		Networks:        networks,
 		NodeGroups:      nodegroups,
 		EventGenerators: eventgenerators,
